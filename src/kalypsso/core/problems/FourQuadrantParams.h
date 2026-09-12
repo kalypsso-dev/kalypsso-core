890d39535dfb6dfbaf2a9abece5170356b804e35
Pierre Kestener <pierre.kestener@cea.fr>
2026-09-12 20:11:42 +0200

    Slightly refactor struct FourQuadrantParams.

--- src/kalypsso/core/problems/FourQuadrantParams.h
+++ src/kalypsso/core/problems/FourQuadrantParams.h
@@ -10,6 +10,7 @@
 
 #include <kalypsso/utils/config/ConfigMap.h>
 #include <kalypsso/core/real_type.h>
+#include <kalypsso/core/enums.h>
 
 namespace kalypsso
 {
@@ -20,7 +21,7 @@ namespace kalypsso
 struct FourQuadrantParams
 {
 
-  Kokkos::Array<real_t, 3> pos;           //! Discontinutity locations
+  Kokkos::Array<real_t, 3> pos;           //! Discontinuity locations
   int                      config_number; //! Config number
 
   FourQuadrantParams(ConfigMap const & config_map)
@@ -29,6 +30,111 @@ struct FourQuadrantParams
     pos[1] = config_map.getReal("four_quadrant", "y", KALYPSSO_NUM(0.8));
     pos[2] = config_map.getReal("four_quadrant", "z", KALYPSSO_NUM(0.8));
     config_number = config_map.getInteger("four_quadrant", "config_number", 0);
+  }
+
+  /**
+   * Get region id from a given location (2d version)
+   *
+   *
+   * region labeling
+   *
+   *  1 | 0
+   *  -----
+   *  2 | 3
+   */
+  template <size_t dim>
+  KOKKOS_INLINE_FUNCTION int
+  get_region_id(Kokkos::Array<real_t, dim> const & xyz) const
+  {
+    int region_id = -1;
+
+    if constexpr (dim == 2)
+    {
+      const real_t & x = xyz[IX];
+      const real_t & y = xyz[IY];
+
+      if (x < pos[IX])
+      {
+        if (y < pos[IY])
+        {
+          region_id = 2;
+        }
+        else
+        {
+          region_id = 1;
+        }
+      }
+      else
+      {
+        if (y < pos[IY])
+        {
+          region_id = 3;
+        }
+        else
+        {
+          region_id = 0;
+        }
+      }
+    }
+    else if constexpr (dim == 3)
+    {
+      const real_t & x = xyz[IX];
+      const real_t & y = xyz[IY];
+      const real_t & z = xyz[IZ];
+
+      if (x < pos[IX])
+      {
+        if (y < pos[IY])
+        {
+          if (z < pos[IZ])
+          {
+            region_id = 2;
+          }
+          else
+          {
+            region_id = 6;
+          }
+        }
+        else
+        {
+          if (z < pos[IZ])
+          {
+            region_id = 1;
+          }
+          else
+          {
+            region_id = 5;
+          }
+        } // end y
+      }
+      else
+      {
+        if (y < pos[IY])
+        {
+          if (z < pos[IZ])
+          {
+            region_id = 3;
+          }
+          else
+          {
+            region_id = 7;
+          }
+        }
+        else
+        {
+          if (z < pos[IZ])
+          {
+            region_id = 0;
+          }
+          else
+          {
+            region_id = 4;
+          }
+        } // end y
+      } // end x
+    } // end dim == 3
+
+    return region_id;
   }
 
 }; // struct FourQuadrantParameters
